8ba46c8b1b931773c945b34b0bf19ab61220c24f
Garett Bare <garett.bare@gmail.com>
2022-07-22 09:27:13 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import LandingPage from './Pages/LandingPage/LandingPage';
+// import SideBar from './Components/SideBar/SideBar.js';
+// import DataSearch from './Pages/DataSearch/DataSearch';
+// import ProductInformation from './Pages/ProductInformation/ProductInformation';
+// import Subimage from './Pages/Subscriptions/Sub-image/Sub-image';
+// import Subscription_Main from './Pages/Subscriptions/Subscription_Main/Subscription_Main';
 
+let test = require("../package.json")
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App"><Routes>
+    {/* <Route
+      path={`${test.name}/datasearch`}
+      element={
+        <DataSearch />
+      }
+    />
+    <Route
+      path={`${test.name}/Subimage`}
+      element={
+        <Subimage />
+      }
+    /> */}
+    <Route
+      path={`${test.name}/`}
+      element={
+        <LandingPage />
+      }
+    />
+    {/* <Route
+      path={`${test.name}/:Product_definition/Product_information`}
+      element={
+        <ProductInformation />
+      }
+    />
+    <Route
+      path={`${test.name}/subscriptions`}
+      element={
+        <Subscription_Main/>
+      }
+    /> */}
+  </Routes>
+  <div>
+      {/* <SideBar name={`${test.name}`}/> */}
+  </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +59,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Pages/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import "./LandingPage.css"
+const LandingPage = (props) => {
+    return ( 
+    <div className='test'>
+        Data Search<br/>
+        Manage Products<br/>
+        Subscriptions<br/> 
+    </div> );
+}
+ 
+export default LandingPage;
